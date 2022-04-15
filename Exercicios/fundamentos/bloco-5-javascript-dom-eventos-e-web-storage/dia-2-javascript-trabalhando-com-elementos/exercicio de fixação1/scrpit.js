f841d4a07b20d748715ada0a455158e737ac1edd
helderdsa <helder_dsa@hotmail.com>
2022-04-15 11:36:38 -0300

    exercicio do dia finalizado

--- Exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio de fixação1/scrpit.js
+++ Exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio de fixação1/scrpit.js
@@ -1,8 +1,30 @@
 document.querySelector("#elementoOndeVoceEsta").parentElement.style.color = "red"
-document.querySelector("#elementoOndeVoceEsta").firstElementChild.innerHTML = "primeiro filho do filho"
+document.querySelector("#elementoOndeVoceEsta").firstElementChild.innerHTML = "--primeiro filho do filho"
 document.querySelector("#pai").firstElementChild.innerHTML = "primeiro filho"
 document.querySelector("#elementoOndeVoceEsta").previousElementSibling.style.color = "blue"
 console.log(document.querySelector("#elementoOndeVoceEsta").nextSibling);
 console.log(document.querySelector("#elementoOndeVoceEsta").nextSibling.nextSibling);
-console.log(document.querySelector("#pai").childNodes[document.querySelector("#pai").childNodes.length-2]);
+console.log(document.querySelector("#pai").childNodes[document.querySelector("#pai").childNodes.length-4]);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let pai = document.querySelector("#pai")
+let irmao = document.createElement("section")
+irmao.innerHTML = "helder"
+pai.appendChild(irmao)
+
+let ondeVE = document.querySelector("#elementoOndeVoceEsta")
+let filhoOnde = document.createElement("div")
+filhoOnde.innerHTML = "--opa, sou o filho"
+ondeVE.appendChild(filhoOnde)
+
+let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho")
+let filhoDoFilho = document.createElement("div")
+filhoDoFilho.innerHTML = "---eu sou filho do filho"
+let filhoDoFilhoID = document.createAttribute("id")
+filhoDoFilhoID.value = "filhoDoFilho"
+filhoDoFilho.setAttributeNode(filhoDoFilhoID)
+primeiroFilhoDoFilho.appendChild(filhoDoFilho)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
